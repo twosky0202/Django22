74b6962b20aa2e3182d4866b4ef09832a080e391
twosky0202 <leehaneul0202@naver.com>
2022-09-22 13:13:39 +0900

    Post 모델 생성, 시간설정

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    # 추후 author 작성'
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title} {self.created_at}'
