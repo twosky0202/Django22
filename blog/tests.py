f937e63ae62515dbf320d4fc136fab2bb46a1d7f
twosky0202 <gahaneul0202@gmail.com>
2022-10-20 13:10:09 +0900

    포스트 목록/상세 페이지에 author 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,15 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestView(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.user_kim = User.objects.create_user(username="kim", password="somepassword")
+        self.user_lee = User.objects.create_user(username="lee", password="somepassword")
 
     def test_post_list(self):
         # self.assertEqual(3, 3)
@@ -32,8 +35,8 @@ class TestView(TestCase):
         self.assertIn('아직 게시물이 없습니다.', main_area.text)
 
         #2. Post가 추가
-        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.")
-        post_002 = Post.objects.create(title="두번째 포스트", content="두번째 포스트입니다.")
+        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.", author=self.user_kim)
+        post_002 = Post.objects.create(title="두번째 포스트", content="두번째 포스트입니다.", author=self.user_lee)
         self.assertEqual(Post.objects.count(), 2)
 
         response = self.client.get('/blog/', follow=True)
@@ -44,8 +47,11 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
 
+        self.assertIn(post_001.author.username.upper(), main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
+
     def test_post_detail(self):
-        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.") #admin에 저장x. test실행 시 잠깐 생겼다가 사라짐
+        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.", author=self.user_kim) #admin에 저장x. test실행 시 잠깐 생겼다가 사라짐
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
         response = self.client.get(post_001.get_absolute_url(), follow=True) # '/blog/1/'
@@ -65,3 +71,4 @@ class TestView(TestCase):
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
         self.assertIn(post_001.content, post_area.text)
+        self.assertIn(post_001.author.username.upper(), post_area.text)
