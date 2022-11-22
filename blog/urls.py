f030a920311eed9066689d06191d1c1de32484eb
twosky0202 <gahaneul0202@gmail.com>
2022-11-22 15:43:23 +0900

    댓글작성폼 구현하기

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,12 @@ from . import views
 urlpatterns = [ # IP주소/blog/
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()), #CBV방식
+    path('<int:pk>/new_comment/', views.new_comment),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('category/<str:slug>/', views.category_page), #IP주소/blog/category/slug/
     path('tag/<str:slug>/', views.tag_page),#IP주소/blog/tag/slug/
     path('create_post/', views.PostCreate.as_view()),
-    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+
 
     #path('', views.index), # IP주소/blog/
     #path('<int:pk>/', views.single_post_page) #FBV(Function Based View)방식
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class PostUpdate(LoginRequiredMixin,UpdateView):
@@ -101,10 +103,28 @@ class PostDetail(DetailView):
         context = super(PostDetail,self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count
+        context['comment_form'] = CommentForm
         return context
 
     # 템플릿은 모델명_detail.html : post_detail.html
     # 매개변수 모델명 : post
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+
+                return redirect(comment.get_absolute_url())
+        else: # GET
+            return redirect(post.get_absolute_url())
+    else: # 로그인 안한 사용자
+        raise PermissionDenied
 
 def category_page(request, slug):
     if slug == 'no_category' :
