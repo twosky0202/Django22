aa58cb98d1a6514f4007894a8240c9cdf6ef5e55
twosky0202 <gahaneul0202@gmail.com>
2022-11-22 16:12:05 +0900

    댓글 수정하기

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [ # IP주소/blog/
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()), #CBV방식
     path('<int:pk>/new_comment/', views.new_comment),
-    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()), #comment의 pk
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()), #post의 pk
     path('category/<str:slug>/', views.category_page), #IP주소/blog/category/slug/
     path('tag/<str:slug>/', views.tag_page),#IP주소/blog/tag/slug/
     path('create_post/', views.PostCreate.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . models import Post, Category, Tag
+from . models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
@@ -125,6 +125,17 @@ def new_comment(request, pk):
             return redirect(post.get_absolute_url())
     else: # 로그인 안한 사용자
         raise PermissionDenied
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    # 템플릿 : comment_form
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate,self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
 
 def category_page(request, slug):
     if slug == 'no_category' :
